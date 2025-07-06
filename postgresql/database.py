10b8173b888c03aacbc68aba735f550c1c519468
Pr0-T0 <sarathsasidharan557@gmail.com>
2025-07-06 21:42:40 +0530

    postgres db with docker

--- postgresql/database.py
+++ postgresql/database.py
@@ -1,3 +1,11 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+import sqlalchemy as _sql
+import sqlalchemy.ext.declarative as _declarative
+import sqlalchemy.orm as _orm
+
+DATABASE_URL = "postgresql://myuser:password@localhost/fastapi_database"
+
+engine = _sql.create_engine(DATABASE_URL)
+
+SessionLocal = _orm.sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = _declarative.declarative_base()
--- postgresql/models.py
+++ postgresql/models.py
@@ -1,1 +1,12 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+import datetime as _dt
+import sqlalchemy as _sql
+import database as _database
+
+class Contact(_database.Base):
+    __tablename__ = "contacts"
+    id = _sql.Column(_sql.Integer,primary_key=True, index=True)
+    first_name = _sql.Column(_sql.String, index=True)
+    last_name = _sql.Column(_sql.String, index=True)
+    email = _sql.Column(_sql.String, index=True, unique=True)
+    phone_number = _sql.Column(_sql.String, index=True, unique=True)    
+    date_created = _sql.Column(_sql.DateTime,default=_dt.datetime.utcnow)
